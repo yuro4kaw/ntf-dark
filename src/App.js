daaebe7a8c8de2f2d8a36f58a1fb8cc7756ed728
Yurii <tahkict.yup4ik@gmail.com>
2024-08-13 19:52:58 +0300

    Project Init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HeaderComponent from "./components/HeaderComponent";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./../styles/App.css"
+
+const HeaderComponent = () => {
+  return (
+    <div className='header container'><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae adipisci dignissimos quisquam rerum ex in, quasi autem eum dolorem nesciunt voluptatibus quia numquam velit doloribus. Vero neque iure reprehenderit voluptatum vel iusto cumque sapiente eaque. Iste quasi consectetur omnis eius eos praesentium ipsa minima fugiat dolore facilis, molestias expedita perferendis.</p></div>
+  )
+}
+
+export default HeaderComponent
