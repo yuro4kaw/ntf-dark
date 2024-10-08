c078835e15a574d87df68e0fca1333a5bc1f6738
Yurii <tahkict.yup4ik@gmail.com>
2024-10-08 18:07:06 +0300

    feature: acf commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,69 +7,91 @@ import ArrowDown from "./../images/icons/ArrowDown.svg";
 import axios from "axios";
 
 const Hero = () => {
-  const [title, setTitle] = useState("");
+  const [heroData, setHeroData] = useState(null);
 
   useEffect(() => {
-    const apiUrl = "http://local.loc/wp-json/wp/v2/posts";
+    const apiUrl =
+      "http://local.loc/wp-json/wp/v2/pages/27?acf_format=standard&_fields=acf,id";
 
     axios
       .get(apiUrl)
       .then((res) => {
-        const result = res.data[0].title.rendered;
-        setTitle(result);
+        const result = res.data.acf;
+        setHeroData(result);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  return (
-    <section className="hero container">
-      <div>
-        <h1>{title}</h1>
-        <div className="hero-buttons">
-          <Button text="Explore" yellow large />
-          <Button text="Create" white large />
+  if (!heroData || !heroData.stats) {
+    return <div>Loading...</div>;
+  }
+  
+  if (heroData.enable_hero) {
+    return (
+      <section className="hero container">
+        <div>
+          <h1>{heroData.hero_title}</h1>
+          <div className="hero-buttons">
+            <Button text={heroData.button1} yellow large />
+            <Button text={heroData.button2} white large />
+          </div>
+          <div className="hero-counters">
+            <Counter
+              value={heroData.stats.result1}
+              description={heroData.stats.text1}
+            />
+            <Counter
+              value={heroData.stats.result2}
+              description={heroData.stats.text2}
+            />
+            <Counter
+              value={heroData.stats.result3}
+              description={heroData.stats.text3}
+            />
+          </div>
         </div>
-        <div className="hero-counters">
-          <Counter value="32k+" description="Artworks" />
-          <Counter value="20k+" description="Auctions" />
-          <Counter value="12k+" description="Creators" />
-        </div>
-      </div>
 
-      <div className="nft-wrapper">
-        <div className="nft-top">
-          <div>
-            <p className="nft-top-text">Current Bid</p>
-            <p className="nft-top-value">0.99 ETH</p>
+        <div className="nft-wrapper">
+          <div className="nft-top">
+            <div>
+              <p className="nft-top-text">Current Bid</p>
+              <p className="nft-top-value">0.99 ETH</p>
+            </div>
+
+            <div>
+              <p className="nft-top-text">Ends in</p>
+              <p className="nft-top-value">25 hrs</p>
+            </div>
           </div>
 
-          <div>
-            <p className="nft-top-text">Ends in</p>
-            <p className="nft-top-value">25 hrs</p>
+          <img
+            className="nft"
+            src={heroData.hero_image}
+            alt="NFT"
+            width={558}
+            height={718}
+          />
+
+          <div className="nft-bottom">
+            <div>
+              <p className="nft-bottom-text">Artist</p>
+              <Link className="nft-bottom-username" to="https://instagram.com">
+                @wzard
+              </Link>
+            </div>
+
+            <Button text="Start Bid" yellow />
           </div>
         </div>
 
-        <img className="nft" src={CardImage} alt="NFT" />
-
-        <div className="nft-bottom">
-          <div>
-            <p className="nft-bottom-text">Artist</p>
-            <Link className="nft-bottom-username" to="https://instagram.com">
-              @wzard
-            </Link>
-          </div>
-
-          <Button text="Start Bid" yellow />
+        <div className="arrow">
+          <img src={ArrowDown} alt="Down" />
         </div>
-      </div>
-
-      <div className="arrow">
-        <img src={ArrowDown} alt="Down" />
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 };
 
 export default Hero;
